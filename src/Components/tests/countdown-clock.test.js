55247d1a9982c03d16f1d067c03f8fb73e8534d7
Opal Thongpetch <crystalclear506@gmail.com>
2021-05-29 00:11:23 +0700

    add test case

--- src/Components/tests/countdown-clock.test.js
+++ src/Components/tests/countdown-clock.test.js
@@ -3,10 +3,36 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import CountdownClock from '../countdown-clock';
 
+jest.useFakeTimers();
 
-test('Component should rendered correctly', () => {
-    const countdownClock = renderer.create(
+test('Component should be rendered correctly', () => {
+    const testRenderer = renderer.create(
         <CountdownClock/>
     );
-    expect(countdownClock).toBeDefined();
+    
+    expect(testRenderer).toMatchSnapshot();
 });
+
+test('Countdown should decrement correctly', (done) => {
+    const testRenderer = renderer.create(
+        <CountdownClock/>
+    );
+
+    const component = testRenderer.getInstance();
+
+    const seconds = 1000 * 60;
+    const timeLater = new Date().getTime() + seconds;
+    component.setCountdownEnd(timeLater);
+    
+    
+    setTimeout(() => {
+        expect(component.state.secondsDisp).toBe(30);
+    }, seconds / 2);
+
+    setTimeout(() => {
+        expect(component.state.secondsDisp).toBe(0);
+    }, seconds);    
+    
+    jest.runAllTimers();
+    done();
+})
